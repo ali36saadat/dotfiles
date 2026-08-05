53b98fb28a25e18509c17c3222d76cd5d0249e0e
Ali Saadat <ali36saadat@gmail.com>
2026-08-05 10:35:26 +0330

    Add hyprland files

---
+++ .config/hypr/modules/misc.lua
@@ -0,0 +1,12 @@
+
+----------------
+----  MISC  ----
+----------------
+
+hl.config({
+    misc = {
+        force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo   = false,
+        font_family = "JetBrainsMono Nerd Font"
+    },
+})
