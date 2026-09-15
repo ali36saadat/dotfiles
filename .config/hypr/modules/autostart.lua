9347b135f406eb31c3aea7f78903cee92110ae5d
Ali Saadat <ali36saadat@gmail.com>
2026-09-15 14:02:54 +0330

    Add clipboard

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -10,4 +10,6 @@
 hl.on("hyprland.start", function ()
   hl.exec_cmd("awww-daemon")
   hl.exec_cmd("quickshell")
+  hl.exec_cmd("wl-paste --type text --watch cliphist store")
+  hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
