449d72bcdc781101e4589dc38db1c830fc575cf6
Ali Saadat <ali36saadat@gmail.com>
2026-09-16 16:29:30 +0330

    Add hyplock and hypridle

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -8,6 +8,7 @@
 -- Or execute your favorite apps at launch like this:
 
 hl.on("hyprland.start", function ()
+  hl.exec_cmd("hypridle")
   hl.exec_cmd("awww-daemon")
   hl.exec_cmd("quickshell")
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
